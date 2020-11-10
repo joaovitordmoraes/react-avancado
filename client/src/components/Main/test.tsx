4ba701e19b2085965b5d2d2b2fb823c66e4e7411
Joao <joaovitordmoraes@gmail.com>
2020-11-10 13:52:52 -0300

    Update project to yarn

--- client/src/components/Main/test.tsx
+++ client/src/components/Main/test.tsx
@@ -3,11 +3,13 @@ import { render, screen } from '@testing-library/react'
 import Main from '.'
 
 describe('<Main />', () => {
-    it('should render the heading', () => {
-        render(<Main />)
+  it('should render the heading', () => {
+    const { container } = render(<Main />)
 
-        expect(
-            screen.getByRole('heading', { name: /react avançado/i })
-        ).toBeInTheDocument()
-    })
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
 })
